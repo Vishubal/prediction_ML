1dea89420db15031c45744594ff18fd322da51c7
Vishubal <vishubal@gmail.com>
2025-02-07 12:30:19 -0600

    set of server files

---
+++ apps.py
@@ -0,0 +1,57 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn
+import nest_asyncio
+from pyngrok import ngrok
+import numpy as np
+from fastapi.middleware.cors import CORSMiddleware
+
+
+# Initialize FastAPI app
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Load the saved model and scaler
+model = pickle.load(open('./src/prediction_models/car_purchase_decision_model.pkl', 'rb'))
+scaler = pickle.load(open('./src/prediction_models/car_purchase_decision_scaler.pkl', 'rb'))
+
+
+# Define input schema
+class CarPurchase(BaseModel):
+  features: list
+
+# Welcome Endpoint
+@app.get("/")
+def read_root():
+  return {"message": "Welcome to the Model Prediction API! ✔"}
+
+# Define prediction endpoint
+@app.post("/predict/car_purchase")
+def predict_car_purchase(input_data: CarPurchase):
+  # Convert input list to NumPy array
+  input_array = np.array(input_data.features).reshape(1, -1)
+
+  # scale the input features
+  scaled_data = scaler.transform(input_array)
+
+  # Make prediction
+  prediction = model.predict(scaled_data)
+  result = "Good Wine" if prediction[0] == 1 else "Bad Wine"
+
+  return {"prediction": result}
+
+# Run the server
+nest_asyncio.apply()
+
+uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
